b5393b2dc3b7946974838b9231f6f1295793f6d9
UnKinSeong <58870474+UnKinSeong@users.noreply.github.com>
2023-02-16 11:49:49 +0000

    asdf

--- Prisma_Database/script.ts
+++ Prisma_Database/script.ts
@@ -1,16 +1,38 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 const prisma = new PrismaClient()
 import express from "express";
 
-const app = express();
+const api = express();
 
-app.use(express.json());
+api.use(express.json());
 
 const port = process.env.PORT || "3000";
 
+api.get("/", async (req: any, res: { json: (arg0: User[]) => void; }) => {
+  const allUsers = await prisma.user.findMany();
+  res.json(allUsers);
+});
 
 
-app.listen(port, () => {
+/*
+  api/auth/signup
+  end point for user to sign up
+*/
+
+
+
+
+
+/*
+  api/auth/login
+  end point for user to login
+*/
+
+
+
+
+
+api.listen(port, () => {
   console.log(`Server Running at ${port} 🚀`);
 });
 
